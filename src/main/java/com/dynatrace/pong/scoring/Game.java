f6bea43d99d548715cf856401ec9e9e4fa410e00
Mirel <mirelle.soodla@gmail.com>
2026-03-19 13:09:08 +0200

    added players

--- src/main/java/com/dynatrace/pong/scoring/Game.java
+++ src/main/java/com/dynatrace/pong/scoring/Game.java
@@ -2,7 +2,5 @@ package com.dynatrace.pong.scoring;
 
 public class Game {
 
-    public int WhoScored(int player1Score, int player2Score){
-        if
-    }
+
 }
--- src/main/java/com/dynatrace/pong/scoring/Scoring.java
+++ src/main/java/com/dynatrace/pong/scoring/Scoring.java
@@ -1,14 +1,18 @@
 package com.dynatrace.pong.scoring;
+import com.dynatrace.pong.model.Player;
 
 public class Scoring {
     private int player1Score;
     private int player2Score;
-    private String player1Name;
-    private String player2Name;
+    private Player player1;
+    private Player player2;
 
     public Scoring(){
         player1Score = 0;
         player2Score = 0;
+        this.player1 = player1;
+        this.player2 = player2;
+
     }
 
     public int getPlayer1Score(){
@@ -41,19 +45,29 @@ public class Scoring {
         this.player2Score = player2Score;
     }
 
-    public int getWinner(int player1Score, int player2Score){
+    public String getWinner(int player1Score, int player2Score){
         if (isGameOver()){
             if (player1Score > player2Score+1 && player1Score >= 11){
                 //System.out.println("Player 1 wins");
-                return ;
+                return player1Name;
             }
             if (player2Score > player1Score+1 && player2Score >= 11){
                 //System.out.println("Player 2 wins");
-                return true;
+                return player2Name;
             }
-            return false;
 
         }
+        return null;
+    }
+
+    public int Player1Scored(){
+        setPlayer1Score(player1Score+1);
+        return player1Score;
+    }
+
+    public int Player2Scored(){
+        setPlayer1Score(player2Score+1);
+        return player2Score;
     }
 
 
